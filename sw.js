e3d2bb172b677b6c1cfc147782414f89fd882de1
anssgnt <59947648+anssgnt@users.noreply.github.com>
2026-05-04 19:30:54 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,6 +10,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
@@ -42,6 +43,6 @@ self.addEventListener('activate', event => {
         cacheNames.filter(name => name !== CACHE_NAME)
           .map(name => caches.delete(name))
       );
-    })
+    }).then(() => self.clients.claim())
   );
 });
